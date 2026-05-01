d0f1d033af4ac53d5ab9cdaaebd9032658302c87
Antonyamose <antonyamose3@gmail.com>
2026-05-01 18:08:08 +0530

    folder-add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import About from './components/About'
+import Steps from './components/Steps'
+import Mentor from './components/Mentor'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+      <Hero/>
+      <About/>
+      <Steps/>
+      <Mentor/>
+      <Footer/>
+    </div>
   )
 }
 
