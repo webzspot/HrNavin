92367a045cf48f5f22b7b8742a9f792008d0ba0e
Antonyamose <antonyamose3@gmail.com>
2026-05-08 12:20:04 +0530

    upd3

--- src/components/Mentor.jsx
+++ src/components/Mentor.jsx
@@ -328,10 +328,10 @@ export default function CoverflowCarousel() {
         >
 
           {/* LEFT FADE */}
-          <div className="absolute left-0 top-0 h-full w-28 lg:w-36 bg-gradient-to-r from-white to-transparent z-10 pointer-events-none" />
+          <div className="absolute left-0 top-0 h-full w-12 lg:w-36 bg-gradient-to-r from-white to-transparent z-10 pointer-events-none" />
 
           {/* RIGHT FADE */}
-          <div className="absolute right-0 top-0 h-full w-28 lg:w-36 bg-gradient-to-l from-white to-transparent z-10 pointer-events-none" />
+          <div className="absolute right-0 top-0 h-full w-12 lg:w-36 bg-gradient-to-l from-white to-transparent z-10 pointer-events-none" />
 
           {/* TOP CURVE */}
           <div className="absolute w-full h-full -top-6 md:-top-24 lg:-top-32 xl:-top-50 z-10">
